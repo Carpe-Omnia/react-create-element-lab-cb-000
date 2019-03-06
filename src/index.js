ff67695df20d19b72d5221197678892ca835d179
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 18:18:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const list = 
-  React.createElement('ul', {class: "my-interests"}, 
+const list =
+  React.createElement('ul', {class: "my-interests"},
 [
   React.createElement('li',{},'Javascript'),
   React.createElement('li',{},'React'),

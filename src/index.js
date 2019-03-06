a2c0e6d27047ae15ff1cc4dff9955311d69c7ffc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 18:34:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const list =
   React.createElement('li',{},'Movies'),
   React.createElement('li',{},'Ice Cream')
 ])
+const header = React.createElement('h1',{},'An Awesome Person') ; 
+const paragraph = React.createElement('p',{},'Who is learning React')
+
 const meInReact =
   React.createElement('div', {class: "me"}, list)
 
